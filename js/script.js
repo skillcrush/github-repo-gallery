1e6a38a6a5525ecbd8614a37b631fd45a7e7c1bb
Ann Cascarano <a.cascarano@gmail.com>
2021-03-16 15:40:11 -0400

    first attempt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const intro = document.querySelector(".intro");
+const overview = document.querySelector(".overview");
 const repoList = document.querySelector(".repo-list");
 const backButton = document.querySelector(".back");
+const allReposContainer = document.querySelector(".repos");
+const repoDataContainer = document.querySelector(".repo-data");
 const filterInput = document.querySelector(".filter-repos");
 const getReposButton = document.querySelector(".get-repos");
 const ghUsername = "redrambles";
@@ -18,21 +20,23 @@ const displayUserInfo = function (data) {
   const div = document.createElement("div");
   div.classList.add("user-info");
   div.innerHTML = `
-    <p>${data.name}</p>
-    <p>${data.bio}</p>
-    <p>${data.location}</p>
-    <p>Number of public repos: ${data.public_repos}</p>
     <figure>
-      <img class="user-avatar" alt="user avatar" src=${data.avatar_url} />
+      <img alt="user avatar" src=${data.avatar_url} />
     </figure>
+    <div>
+      <p><strong>Username:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
   `;
-  intro.append(div);
+  overview.append(div);
   getReposButton.classList.remove("hide");
 };
 
 const gitRepos = async function (ghUsername) {
+  getReposButton.classList.add("hide");
   const fetchRepos = await fetch(`https://api.github.com/users/${ghUsername}/repos`);
-  console.log(`fetchRepos = ${fetchRepos}`);
   const repoData = await fetchRepos.json();
   displayRepos(repoData);
 };
@@ -45,58 +49,71 @@ const displayRepos = function (repos) {
   // Grab info about the GitHub user to display on left hand side of list
   for (const repo of repos) {
     const repoItem = document.createElement("li");
-    repoItem.innerText = "something";
-    // repoList.append(repoItem);
-    console.log(repo);
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `
+      <h3>${repo.name}</h3>
+      <p>Main language: ${repo.language}</p>
+      `;
+    repoList.append(repoItem);
+    // console.log(repo);
   }
-
-  // for (const repo of repos) {
-  //   const repoItem = document.createElement("div");
-  //   repoItem.classList.add("repo");
-  //   repoItem.innerHTML = `
-  //       <h3>${repo.name}</h3>
-  //       <div class="extra-info">
-  //           <p>Main language: ${repo.language}</p>
-  //           <p class="description">Description: ${repo.description}</p>
-  //           <a class="visit" href=${repo.html_url} target="_blank">Visit Repo </a>
-  //       </div>
-  //   `;
-  //   repoContainer.append(repoItem);
-  // }
 };
 
-// // Click on a user to know more
-// followingContainer.addEventListener("click", function (e) {
-//   if (e.target.matches("h2.repo-owner")) {
-//     const username = e.target.innerText;
-//     fetchUserRepos(username);
-//     followingContainer.classList.add("hide");
-//     discoverContainer.classList.remove("hide");
-//     filterInput.classList.remove("hide");
-//   }
-// });
+// Click on a repo to see more details
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const reponame = e.target.innerText;
+    getRepoInfo(reponame);
+  }
+});
 
-// backButton.addEventListener("click", function () {
-//   followingContainer.classList.remove("hide");
-//   discoverContainer.classList.add("hide");
-//   backButton.classList.add("hide");
-//   filterInput.classList.add("hide");
-//   filterInput.value = "";
-// });
+const getRepoInfo = async function (reponame) {
+  const fetchInfo = await fetch(`https://api.github.com/repos/${ghUsername}/${reponame}`);
+  const repoData = await fetchInfo.json();
+  displayRepoInfo(repoData);
+};
 
-// // Dynamic search
-// filterInput.addEventListener("input", function (e) {
-//   const searchText = e.target.value;
-//   const repos = document.querySelectorAll(".repo");
+const formatDate = function (raw_date) {
+  const timeStamp = Date.parse(raw_date);
+  const date = new Date(timeStamp).toDateString();
+  return date;
+};
 
-//   for (const repo of repos) {
-//     if (!repo.innerText.toLowerCase().includes(searchText.toLowerCase())) {
-//       repo.classList.add("hide");
-//     } else {
-//       repo.classList.remove("hide");
-//     }
-//   }
-// });
+const displayRepoInfo = function (repoData) {
+  backButton.classList.remove("hide");
+  repoDataContainer.innerHTML = "";
+  repoDataContainer.classList.remove("hide");
+  allReposContainer.classList.add("hide");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>Name: ${repoData.name}</h3>
+    <p>Default branch: ${repoData.default_branch}</p>
+    <p>Description: ${repoData.description}</p>
+    <p>Last Updated: ${formatDate(repoData.pushed_at)}</p>
+    <a class="visit" href="${repoData.html_url}" target="_blank" rel="noreferrer noopener">Visit Repo on GitHub :)</a>
+  `;
+  repoDataContainer.append(div);
+};
+
+backButton.addEventListener("click", function () {
+  allReposContainer.classList.remove("hide");
+  repoDataContainer.classList.add("hide");
+  backButton.classList.add("hide");
+});
+
+// Dynamic search
+filterInput.addEventListener("input", function (e) {
+  const searchText = e.target.value;
+  const repos = document.querySelectorAll(".repo");
+
+  for (const repo of repos) {
+    if (!repo.innerText.toLowerCase().includes(searchText.toLowerCase())) {
+      repo.classList.add("hide");
+    } else {
+      repo.classList.remove("hide");
+    }
+  }
+});
 
 // !!! Unauthenticated clients can make 60 requests per hour !!! To get more requests per hour, we'll need to authenticate. In fact, doing anything interesting with the GitHub API requires authentication. Learn more about how to do that here
 
