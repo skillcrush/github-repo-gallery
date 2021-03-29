5c65fd21d101663559192e038604cce627a08bac
Ann <a.cascarano@gmail.com>
2021-03-29 12:11:56 -0400

    Remove main language from repos call

--- js/script.js
+++ js/script.js
@@ -38,10 +38,7 @@ const displayRepos = function (repos) {
   for (const repo of repos) {
     const repoItem = document.createElement("li");
     repoItem.classList.add("repo");
-    repoItem.innerHTML = `
-      <h3>${repo.name}</h3>
-      <p>Main language: ${repo.language}</p>
-      `;
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
     repoList.append(repoItem);
   }
 };
