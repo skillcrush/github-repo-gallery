25134ae927dbbda6082e66179d00b0a22a24373a
Ann <a.cascarano@gmail.com>
2021-03-29 12:12:15 -0400

    Adjust placeholder

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const displayUserInfo = function (data) {
       <img alt="user avatar" src=${data.avatar_url} />
     </figure>
     <div>
-      <p><strong>Username:</strong> ${data.name}</p>
+      <p><strong>Name:</strong> ${data.name}</p>
       <p><strong>Bio:</strong> ${data.bio}</p>
       <p><strong>Location:</strong> ${data.location}</p>
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
