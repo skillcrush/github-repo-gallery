a00af5a67b757a3d57331e1712f212ef45f92388
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-06-10 18:26:52 -0400

    Completes MVP with function to filter list of all repos, by name

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const fetchUser = async function(){
 
 fetchUser();
 
-
+//display basic data about the featured GitHub user
 const showUserData = function(userData){
     const userInfo = document.createElement("div");
     userInfo.classList.add("user-info")
@@ -33,7 +33,7 @@ const showUserData = function(userData){
     const name = userData.name;
     const bio = userData.bio;
     const location = userData.location;
-    const repos = userData.public_repos;
+    const publicRepos = userData.public_repos;
     userInfo.innerHTML = `
         <figure>
             <img alt="user avatar" src=${avatar} />
@@ -42,21 +42,24 @@ const showUserData = function(userData){
             <p><strong>Name:</strong> ${name}</p>
             <p><strong>Bio:</strong> ${bio}</p>
             <p><strong>Location:</strong> ${location}</p>
-            <p><strong>Number of public repos:</strong> ${repos}</p>
+            <p><strong>Number of public repos:</strong> ${publicRepos}</p>
         </div> 
     `;
     overview.append(userInfo);
 };
 
+//fetch all repos of this user, sorted by most recently updated
 const fetchRepos = async function (){
     const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
-    const repos = await response.json();
+    const allRepos = await response.json();
     //console.log(repos);
-    displayReposList(repos);
+    displayReposList(allRepos);
     
 };
 
+//display the fetched repos
 const displayReposList = function (repos){ //not sure about this parameter
+    filterInput.classList.remove("hide");
     for (const repo of repos) { // or this const
         const li = document.createElement("li");
         li.classList.add("repo");
@@ -68,7 +71,8 @@ const displayReposList = function (repos){ //not sure about this parameter
 
 fetchRepos();
 
-const repoList = repoListElement.addEventListener("click", function(e){ //can you name an event listener?
+//listen for click on a single repo's name
+const repoList = repoListElement.addEventListener("click", function(e){ //can you name an event listener? Apparently yes.
     if (e.target.matches("h3")) {
         const repoName = e.target.innerText; //shouldn't reuse variable names
         fetchSingleRepoData(repoName);
@@ -76,7 +80,8 @@ const repoList = repoListElement.addEventListener("click", function(e){ //can yo
 
 });
 
-const fetchSingleRepoData = async function (repoName){ //why can we call for this? it's only defined inside that if statement.  So...probably this function will also get called inside the if statement?
+//grab data for the clicked-on single repo
+const fetchSingleRepoData = async function (repoName){ //why can we call for this parameter? it's only defined inside that if statement.  So...probably this function will also get called inside the if statement? Update: Yep, that's what happened.
     const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
     const repoInfo = await response.json();
     console.log(repoInfo);
@@ -91,7 +96,7 @@ const fetchSingleRepoData = async function (repoName){ //why can we call for thi
     displaySingleRepoInfo(repoInfo, languages);
 };
 
-
+//display some stats about the selected repo
 const displaySingleRepoInfo = function (repoName, languages){
     repoDataElement.innerHTML = "";
     const singleRepoDiv = document.createElement("div"); 
@@ -112,8 +117,28 @@ const displaySingleRepoInfo = function (repoName, languages){
     viewReposButton.classList.remove("hide");
 };
 
+//button to leave single repo stats and return to full list of repos
 viewReposButton.addEventListener("click", function(){
     repoDataElement.classList.add("hide");
     allReposElement.classList.remove("hide");
     viewReposButton.classList.add("hide");
 });
+
+//search box to filter repos by name
+filterInput.addEventListener("input", function(e){
+    const searchText = e.target.value;
+    const lowerSearchText = searchText.toLowerCase();
+    const repos = document.querySelectorAll(".repo");
+    //console.log(repos);
+    //console.log(lowerSearchText);
+    for (const repo of repos) {
+        let lowerRepoName = repo.innerText.toLowerCase();
+        if (lowerRepoName.includes(lowerSearchText)) { 
+            repo.classList.remove("hide");
+        } else {
+            repo.classList.add("hide");
+        }  
+    }
+});
+
+//to do: add functionality to filter by language
