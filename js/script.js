4e1d3476d66c8cbe3d2ce892ff261c80e4b528ba
laura_m <lauramarkus2@gmail.com>
2021-11-05 18:56:22 +0100

    function to display user info

--- js/script.js
+++ js/script.js
@@ -8,7 +8,31 @@ const getInfo = async function(){
     const request = await fetch (`https://api.github.com/users/${username}`);
     const data = await request.json();
     console.log(data);
+    
+    displayUserInfo(data);
+
 };
 getInfo();
 
+const displayUserInfo = function (data){
+    const avatar = data.avatar_url;
+    const name = data.name;
+    const bio = data.bio;
+    const location = data.location;
+    const numRepos = data.public_repos;
 
+
+    const infoDiv = document.createElement("div");
+        infoDiv.classList.add("user-info");
+        infoDiv.innerHTML= `
+        <figure> 
+            <img src=${avatar} alt="user avatar" /> </figure>
+        <div>
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Bio:</strong> ${bio}</p>
+            <p><strong>Location:</strong> ${location}</p>
+            <p><strong>Number of public repos:</strong> ${numRepos}</p>
+        </div> 
+        `;
+        profileInfo.append(infoDiv);
+};
