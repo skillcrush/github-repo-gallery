274688953dfa911091e13c402eaeb3e99ed817e9
laura_m <lauramarkus2@gmail.com>
2021-11-08 19:08:38 +0100

    adds async function to fetch repo info from API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //targets the DIV where profile info will appear
 const profileInfo = document.querySelector(".overview");
-
+const repoList = document.querySelector(".repo-list");
 const username = "lauramarkus";
+
 
 //async function to fetch info from github profile
 const getInfo = async function(){
@@ -16,7 +17,7 @@ getInfo();
 
 const displayUserInfo = function (data){
     const avatar = data.avatar_url;
-    const name = data.name;
+    const myname = data.name;
     const bio = data.bio;
     const location = data.location;
     const numRepos = data.public_repos;
@@ -28,7 +29,7 @@ const displayUserInfo = function (data){
         <figure> 
             <img src=${avatar} alt="user avatar" /> </figure>
         <div>
-            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Name:</strong> ${myname}</p>
             <p><strong>Bio:</strong> ${bio}</p>
             <p><strong>Location:</strong> ${location}</p>
             <p><strong>Number of public repos:</strong> ${numRepos}</p>
@@ -36,3 +37,14 @@ const displayUserInfo = function (data){
         `;
         profileInfo.append(infoDiv);
 };
+
+//async function to fetch my github repos
+const getRepos = async function (){
+    const repoRequest = await fetch (`https://api.github.com/users/${username}/repos?direction"asc&per_page=100`);
+    const repoData = await repoRequest.json();
+    console.log(repoData);
+};
+getRepos(); 
+
+
+    
