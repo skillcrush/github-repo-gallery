a1bff5c3f94bf3210c7f5b3c570383988d8bde87
tauri_s <tauristclaire@gmail.com>
2026-08-10 17:27:25 -0700

    Change repoInfo div to have repo-details classname and change div variable name to be more descriptive

--- js/script.js
+++ js/script.js
@@ -73,13 +73,14 @@ const displayRepoInfo = function (repoInfo, languages) {
   repoData.innerHTML = "";
   repoData.classList.remove("hide");
   allReposContainer.classList.add("hide");
-  const div = document.createElement("div");
-  div.innerHTML = `
+  const repoDetails = document.createElement("div");
+  repoDetails.classList.add("repo-details");
+  repoDetails.innerHTML = `
     <h3>Name: ${repoInfo.name}</h3>
     <p>Description: ${repoInfo.description}</p>
     <p>Default Branch: ${repoInfo.default_branch}</p>
     <p>Languages: ${languages.join(", ")}</p>
     <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
   `;
-  repoData.append(div);
+  repoData.append(repoDetails);
 };
